c91bf3da424e81bd51d2f7fd60af61b8d4db7070
ClickManer <slava.metelicza@bk.ru>
2025-05-18 02:19:32 +0300

    the second task

--- first.js
+++
@@ -1,9 +0,0 @@
-function hoistingTest() {
-    console.log(x)
-    console.log(y)
-    var x
-    let y
-    console.log(x)
-    console.log(y)
-}
-hoistingTest()
---
+++ two.js
@@ -0,0 +1,7 @@
+const a = 1;
+
+if (true) {
+	var b = 2;
+}
+
+console.log(a + b); // 3
